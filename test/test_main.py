b7581a61d21b331d7558119d679473f96f5d5e61
divyaprabha <saravudivya@gmail.com>
2026-09-21 15:52:47 +0530

    ci-cd-testing-pipeline

---
+++ test/test_main.py
@@ -0,0 +1,28 @@
+from src.main import add
+from src.main import subtract
+from src.main import multiply
+from src.main import divide
+
+
+def test_add_numbers():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+
+def test_subtract_numbers():
+    assert subtract(5, 3) == 2
+    assert subtract(1, 1) == 0
+    assert subtract(0, 0) == 0
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 0) == 0
+
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(1, 1) == 1
+    assert divide(0, 1) == 0
